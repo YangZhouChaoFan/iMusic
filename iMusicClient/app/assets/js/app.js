1f5243d43c4baba0bc3e82dcf613ff440c5b08bd
chenhao <chenhao1000@live.cn>
2016-01-07 10:01:41 +0800

    创建项目

--- iMusicClient/app/assets/js/app.js
+++ iMusicClient/app/assets/js/app.js
@@ -1,28 +1,53 @@
-(function(nw){
+(function (nw) {
     var app = angular.module('app', ['ngMaterial']);
-    app.config(function($mdThemingProvider) {
+    app.config(function ($mdThemingProvider) {
+        //主题设置
         $mdThemingProvider.theme('default')
             .primaryPalette('teal')
-            //.accentPalette('orange');
+        //.accentPalette('orange');
     });
-    app.controller('toolBarCtrl', function($scope, $mdSidenav){
-        $scope.toggleSliderBar = function(){
+    app.controller('toolBarCtrl', function ($scope, $mdSidenav) {
+
+        //滑动栏
+        $scope.toggleSliderBar = function () {
             $mdSidenav('left').toggle();
         };
 
-        $scope.minWindow = function(){
+        //最小化、还原、最大化、隐藏、托盘
+        tray = new nw.Tray({title: 'iMusic', icon: 'app/assets/img/logo.png'});
+        tray.on('click', function(){
+            nw.Window.get().show();
+        });
+        menu = new nw.Menu();
+        menu.append(new nw.MenuItem({type: 'checkbox', label: '允许通知'}));
+        menu.append(new nw.MenuItem({type: 'separator'}));
+        menu.append(new nw.MenuItem({
+            label: '打开iMusic',
+            click: function(){
+                nw.Window.get().show();
+            }
+        }));
+        menu.append(new nw.MenuItem({
+            label: '关闭iMusic',
+            click: function(){
+                nw.Window.get().close();
+            }
+        }));
+        tray.menu = menu;
+        $scope.isNormal = true;
+        $scope.minWindow = function () {
             nw.Window.get().minimize();
         };
-
-        $scope.maxWindow = function(){
-            nw.Window.get().maximize();
+        $scope.maxWindow = function () {
+            if ($scope.isNormal) {
+                nw.Window.get().maximize();
+            } else {
+                nw.Window.get().unmaximize()
+            }
+            $scope.isNormal = !$scope.isNormal
         };
-
-        $scope.closeWindow = function(){
-            nw.Window.get().close();
+        $scope.closeWindow = function () {
+            nw.Window.get().hide();
         };
     });
 })(require('nw.gui'));
-
-//red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green,
-//lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey
