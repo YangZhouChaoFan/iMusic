18b5ced2f534d7247538bf8178a09b0505ad15d8
chenhao <chenhao1000@live.cn>
2016-01-07 15:50:48 +0800

    创建项目

--- iMusicClient/app/assets/js/app.js
+++ iMusicClient/app/assets/js/app.js
@@ -51,7 +51,7 @@
         };
     }).controller('songListCtrl', function($scope){
         //歌曲列表
-        $scope.songs = ['1','2','3','4','5','6','7','8','9','10'];
+        $scope.songs = ['1','2','3','4','5','6','7','8','9','10','11','12'];
         $scope.selectSong = function(index){
             $scope.selectItem = index;
         }
