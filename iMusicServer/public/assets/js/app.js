eabc3865cad078af8208b3a559791bdc3eb8911d
chenhao <chenhao1000@live.cn>
2016-01-11 19:38:16 +0800

    服务端创建

--- iMusicServer/public/assets/js/app.js
+++ iMusicServer/public/assets/js/app.js
@@ -1,7 +1,33 @@
-var app = angular.module('app', ['ngMaterial', 'ngAnimate']);
-app.config(function ($mdThemingProvider) {
+var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngRoute']);
+app.config(function ($mdThemingProvider, $routeProvider) {
     //主题设置
     $mdThemingProvider.theme('default')
         .primaryPalette('teal')
         .accentPalette('brown');
+
+    //路由设置
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeCtrl'
+    }).when('/music', {
+        templateUrl: 'music.html',
+        controller: 'musicCtrl'
+    }).when('/user', {
+        templateUrl: 'user.html',
+        controller: 'userCtrl'
+    }).when('/setting', {
+        templateUrl: 'setting.html',
+        controller: 'settingCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    });
 });
+app.controller('homeCtrl',function($scope){
+
+}).controller('musicCtrl',function($scope){
+
+}).controller('userCtrl',function($scope){
+
+}).controller('settingCtrl',function($scope){
+
+})
