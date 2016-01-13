fd772fb1ffc00aff4230b8b391a2bb770695f1dd
chenhao <chenhao1000@live.cn>
2016-01-13 19:20:21 +0800

    服务端创建

--- iMusicServer/services/musicService.js
+++ iMusicServer/services/musicService.js
@@ -1,5 +1,6 @@
 var music = require('../models/musicModel').music;
 
+//查询
 exports.query = function (data, callback) {
     music.find(data, function (err, results) {
         if (err) {
@@ -10,12 +11,14 @@ exports.query = function (data, callback) {
     });
 };
 
+//删除
 exports.delete = function (data, callback) {
     music.remove({_id: {$in: data}}).exec(function (err) {
         callback(err)
     });
 };
 
+//新增
 exports.insert = function (data, callback) {
     var musicData = new music(data);
     musicData.save(function(err){
@@ -23,6 +26,7 @@ exports.insert = function (data, callback) {
     });
 };
 
+//修改
 exports.update = function (data, callback) {
     music.find({_id: data._id}).update(data).exec(function(err){
         callback(err);
