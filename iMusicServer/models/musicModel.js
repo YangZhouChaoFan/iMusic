659489c44a046b6c4379b9784eb0071a387b5b4b
chenhao <chenhao1000@live.cn>
2016-01-13 15:42:13 +0800

    服务端创建

--- iMusicServer/models/musicModel.js
+++ iMusicServer/models/musicModel.js
@@ -1,10 +1,19 @@
 var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/imusic');
 
-var musicSchema = new mongoose.Schema({
+mongoose.connect('mongodb://localhost/imusic');
+
+var db = mongoose.connection;
+
+db.once('open', function callback () {
+    console.log('mongoDB connected...')
+});
+
+var Schema = mongoose.Schema;
+var musicSchema = new Schema({
     name: String,
     author: String,
     type: String
 });
 
 exports.muisc = mongoose.model('music', musicSchema);
+exports.db = db;
